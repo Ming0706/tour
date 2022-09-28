9ee1a65de5bb421fc53f1d7afd515ebfbd3a11a3
Ming0706 <2805001744@qq.com>
2022-09-28 16:22:25 +0800

    change add_test

--- test/add_test.go
+++ test/add_test.go
@@ -1,13 +1,13 @@
 package test
 
-import(
+import (
 	"fmt"
+	"github.com/Ming0706/tour"
 	"testing"
 )
 
 func TestAdd(t *testing.T) {
-
-	r := Add(1, 2)
+	r := tour.Add(1, 2)
 	if r !=3 {
 
 		t.Errorf("Add(1, 2) failed. Got %d, expected 3.", r)
