dc8ab63f0b87391d69262492ff14580df04a1fce
Ming0706 <2805001744@qq.com>
2022-09-29 16:48:17 +0800

    change main

--- main.go
+++ main.go
@@ -5,15 +5,40 @@ import (
 	"log"
 )
 
-func main() {
+/*func main() {
 	var name string
 	flag.StringVar(&name, "name", "Go 语言编程之旅", "帮助信息")
 	flag.StringVar(&name, "n", "Go 语言编程之旅", "帮助信息")
 	flag.Parse()
 
 	log.Printf("name: %s", name)
+}*/
+
+
+
+func main() {
+	var name string
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+
+	switch args[0] {
+	case "go":
+		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+		/* ContinueOnError : 返回错误描述
+		ExitOnError : 调用 os.Exit(2) 退出程序
+		PanicOnError : 调用 panic 语句抛出错误异常 */
+		goCmd.StringVar(&name, "name", "Go 语言", "帮助信息")
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+		phpCmd.StringVar(&name, "n", "PHP 语言", "帮助信息")
+		_ = phpCmd.Parse(args[1:])
+	}
+
+	log.Printf("name: %s", name)
 }
 
-func Main() {
-	println("test")
-}
